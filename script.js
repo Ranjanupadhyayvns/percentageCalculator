57ec3f3ac2446aeff685f8c8f910dc45c4b1065f
Ranjan Upadhyay <138987248+Ranjanupadhyayvns@users.noreply.github.com>
2024-04-01 07:59:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// myFunction = () => {
+//     var result = document.getElementById("result")
+//     let z = document.getElementById("third-input-box");
+//     let xvalue = parseFloat(document.getElementById("first-input-box").value);
+//     let yvalue = parseFloat(document.getElementById("secound-input-box").value);
+//     if (!isNaN(xvalue) && !isNaN(yvalue)) {
+//         z.value = ((xvalue * yvalue) / 100).toFixed("2");
+//         result.innerHTML = `<strong>${xvalue}</strong><em> % of</em><strong> ${yvalue}</strong><em> is </em><strong><mark> ${z.value} </mark></span>`
+//     }
+//     else {
+//         result.innerHTML = "Input can not be empty!";
+//     }
+// }
+// myFunction();
